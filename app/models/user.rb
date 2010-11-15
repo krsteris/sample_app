c399f35d187f9fb21c3b1e1b70928ecc79d8c570
krsteris <kodeetsrke@yahoo.com>
2010-11-15 23:21:18 +0100

    vo ch6

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,9 @@ class User < ActiveRecord::Base
   #class User < ActiveRecord::Base
   #attr_accessible(:name, :email)
   #validates_presence_of(:name)
-
-  attr_accessible :name,:email
+  
+  attr_accessor :password
+  attr_accessible :name,:email,:password, :password_confirmation
 
   validates_presence_of :name,:email
   validates_length_of   :name, :maximum => 50
@@ -30,5 +31,54 @@ class User < ActiveRecord::Base
   #i nema da dozvolva da se zacuva
   validates_uniqueness_of :email, :case_sensitive => false
 
+
+
+  #ch6
+  validates_confirmation_of :password
+  #which will automatically create a virtual attribute called password_confirmation, while confirming that it matches the password attribute at the same time.
+
+
+
+
+  #attr_accessor :password
+  #attr_accessible :password, :password_confirmation
+
+  # Automatically create the virtual attribute 'password_confirmation'.
+  #validates_confirmation_of :password
+
+  # Password validations.
+  validates_presence_of :password
+  validates_length_of   :password, :within => 6..40
+
+
+
+    before_save :encrypt_password
+
+    # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+    encrypted_password == encrypt(submitted_password)
+  end
   
+  private
+
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
+    
 end
